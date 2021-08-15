25f37748407b28db68cbdbdc0d3b471586067775
Swistek92 <swistekxD@gmail.com>
2021-08-15 12:43:15 +0200

    feat login/logout in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import StoreProvider from './store/StoreProvider.jsx';
 import './App.scss';
 import 'regenerator-runtime/runtime'
+import Header from './components/Header/Header'
+
 
 const App = () => (
 
   <StoreProvider>
-    <header>hello</header>
+    <Header />
+    {/* <header>hello</header> */}
+
   </StoreProvider>
 );
 
