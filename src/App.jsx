497a162349f1c4ac9f634f8fc9e3d629164d4d10
Swistek92 <swistekxD@gmail.com>
2021-08-16 04:23:53 +0200

    aside menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import StoreProvider from './store/StoreProvider.jsx';
 import './App.scss';
 import 'regenerator-runtime/runtime'
 import Header from './components/Header/Header'
-
-
+import AsideMenu from './components/AsideMenu/AsideMenu.jsx'
+import { HashRouter as Router } from "react-router-dom";
 const App = () => (
 
   <StoreProvider>
+
     <Header />
-    {/* <header>hello</header> */}
+    <Router>
+      <AsideMenu />
+
+    </Router>
+
 
   </StoreProvider>
 );
